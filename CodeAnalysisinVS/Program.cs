eed14e95f3d271e0c5d1c5d0c8d4fe851f5a9cc2
Sharan-99 <sharan.ceg2017103591@gmail.com>
2022-05-25 09:15:35 +0530

    first commit

---
+++ CodeAnalysisinVS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysisinVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "John","Boby","Charig","Denis","Alfee","Bond" };
+            var namesOfB = names.Where(n => n.StartsWith("B"));
+        }
+    }
+}
